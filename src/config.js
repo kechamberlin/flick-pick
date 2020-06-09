82b8b28892bc13389ccf65c36222e27568859ab3
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-09 13:40:09 -0700

    Added api url, api key, image base url, backdrop size, and poster size variables.

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '105dba84b6da4fd322ae305340e8884c';
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p';
+const BACKDROP_SIZE = 'w1280';
+const POSTER_SIZE = 'w500';
+
+export { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };
